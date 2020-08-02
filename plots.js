8ef287f7e2a2e0e7fab54d4295b8ac77388fb6d0
Argelia  Cardenas <argecardenas@Argelias-Air.attlocal.net>
2020-08-01 23:34:07 -0700

    module activities

---
+++ plots.js
@@ -0,0 +1,42 @@
+function init() {
+    var selector = d3.select("#selDataset");
+  
+    d3.json("samples.json").then((data) => {
+      console.log(data);
+      var sampleNames = data.names;
+      sampleNames.forEach((sample) => {
+        selector
+          .append("option")
+          .text(sample)
+          .property("value", sample);
+      });
+  })}
+  
+  init();
+
+  function optionChanged(newSample) {
+    console.log(newSample);
+  }
+
+  function buildMetadata(sample) {
+    d3.json("samples.json").then((data) => {
+      var metadata = data.metadata;
+      var resultArray = metadata.filter(sampleObj => sampleObj.id == sample);
+      var result = resultArray[0];
+      var PANEL = d3.select("#sample-metadata");
+  
+      PANEL.html("");
+      PANEL.append("h6").text("id: " + result.id);
+      PANEL.append("h6").text("ethnicity: " + result.ethnicity);
+      PANEL.append("h6").text("gender: " + result.gender);
+      PANEL.append("h6").text("age: " + result.age);
+      PANEL.append("h6").text("location: " + result.location);
+      PANEL.append("h6").text("bbtype: " + result.bbtype);
+      PANEL.append("h6").text("wfreq: " + result.wfreq);
+    });
+}
+  
+  function optionChanged(newSample) {
+    buildMetadata(newSample);
+    buildCharts(newSample);
+  }
